e72b4d5ae318895d4144effe2219de3a65ee5e49
hamz384 <ha384@student.le.ac.uk>
2024-09-19 15:42:19 +0100

    Add files via upload

---
+++ q1-msg.sh
@@ -0,0 +1,4 @@
+echo $ ./q1-msg.sh
+echo "Hello! This is the first shell scripting exercise for module CO1101."
+echo "The script is being run by user "$(whoami)
+echo "The current directory is "$PWD
